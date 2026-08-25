ba27eeaed1ee087f0cf7d0763d80b0b2d21fef0e
SocaLatchman <r.latchman3624@gmail.com>
2026-08-25 03:16:13 -0700

    Base class for models and os, dotenv, sqlalchemy imports

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from dotenv import load_dotenv
+import os
 
 
+class Base(DeclarativeBase):
+   def save(self):
+      db.session.add(self)
+      db.session.commit()
+
+   def update(self):
+      db.session.execute(self)
+      db.session.commit()
+       
+load_dotenv('.env')
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+db = SQLAlchemy(app, model_class=Base)
+
+
+
+
 
 
 @app.route('/')
